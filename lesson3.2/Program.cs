7d6203527164db0476b9937d777573115ea7125a
Anton <korjant09996@gmail.com>
2022-10-18 13:49:53 +0400

    Complite 3.2

---
+++ lesson3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному
+// номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти 
+
+
+Console.WriteLine("Укажите номер четверти ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+
+string NomerQuarter(int number)
+{
+    if (number == 1) return "диапазон возможных координат x > 0 y > 0";
+    if (number == 2) return "диапазон возможных координат x < 0 y > 0";
+    if (number == 3) return "диапазон возможных координат x < 0 y < 0";
+    if (number == 4) return "диапазон возможных координат x > 0 y < 0";
+    return "Введен некорректный номер четверти";
+}
+
+string result = NomerQuarter(x);
+Console.WriteLine(result);
