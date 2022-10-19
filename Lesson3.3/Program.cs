d1cb9655e528375a8dc88ef3ca7d3b19fc3da3d1
Anton <korjant09996@gmail.com>
2022-10-19 10:35:20 +0400

    Complite 3.3

---
+++ Lesson3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает
+// на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве 
+// A(3,6); B(2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("укажите координаты первой точки, по оси X");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("укажите координаты первой точки, по оси Y");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("укажите координаты второй точки, по оси X");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("укажите координаты второй точки, по оси Y");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double Hypotenuse(int x1, int y1, int x2, int y2)
+{
+    double result = Math.Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+    return result;
+}
+
+double result = Hypotenuse(x1, y1, x2, y2);
+Console.WriteLine(Math.Round(result,2));
